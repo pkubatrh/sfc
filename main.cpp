387bf07d070390a2fa31952ce3de874620454ed0
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-12-05 23:14:22 +0100

    Added arguments and classification
    Signed-off-by: Petr Kubat <xkubat11@stud.fit.vutbr.cz>

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@
 #include <cmath>
 #include <string>
 #include <algorithm>
+#include <unistd.h>
 
-int rmax = 3;
+struct args {
+    std::ifstream f;
+    bool fflag;
+    bool wolfram;
+    int rmax;
+};
 
 struct hidden_neuron {
     std::string name;
@@ -37,16 +43,61 @@ void print_vector(int *v, int len) {
     std::cout << ")";
 }
 
-int main()
+void read_vector(std::vector<int> *vect, int vector_len) {
+    int n;
+    bool flag = true;
+    std::string input;
+    while (flag) {
+        std::cout << ">";
+        std::getline(std::cin, input);
+        std::istringstream sstr(input);
+        while(sstr >> n) {
+            vect->push_back(n);
+        }
+        if (vect->size() != vector_len) {
+            vect->clear();
+            std::cout << "Wrong vector size." << std::endl;
+        }
+        else {
+            flag = false;
+        }
+    }
+}
+
+int main(int argc, char **argv)
 {
-    std::ifstream f;
+    args a;
+    char c;
     bool modif = true;
     std::vector<hidden_neuron> net;
     std::vector<int> out_neurons;
     std::vector<std::vector<int> > v;
-    f.open("input");
+    // Default radius
+    a.rmax = 3;
+    a.wolfram = false;
+    a.fflag = false;
+    while ((c = getopt (argc, argv, "wr:f:")) != -1) {
+        switch(c) {
+            case 'r':
+                a.rmax = atoi(optarg);
+                break;
+            case 'f':
+                a.f.open(optarg);
+                a.fflag = true;
+                break;
+            case 'w':
+                a.wolfram = true;
+                break;
+            case '?':
+                exit(1);
+        }
+    };
+    if (!a.fflag) {
+        exit(1);
+    }
+
     std::string input;
-    while(std::getline(f, input)) {
+    while(std::getline(a.f, input)) {
         std::vector<int> vect;
         std::istringstream sstr(input);
         int n;
@@ -55,7 +106,7 @@ int main()
         }
         v.push_back(vect);
     }
-    f.close();
+    a.f.close();
     if (v.size() == 0) exit(1);
     int vector_num = v.size();
     int vector_len = v[0].size() -1;
@@ -75,7 +126,7 @@ int main()
                 std::cout << ", class: " << net[j].cls;
                 std::cout << ", distance: " << distance << std::endl;
                 if (distance <= net[j].r) {
-                    if (v[i][2] == out_neurons[net[j].out_neuron]) {
+                    if (v[i][vector_len] == out_neurons[net[j].out_neuron]) {
                         std::cout << "    Hit - matching class" << std::endl;
                         hit = true;
                     } else {
@@ -91,8 +142,8 @@ int main()
                 hidden_neuron new_neuron;
                 new_neuron.name = "Neuron_" + std::to_string(i+1);
                 new_neuron.pv = &v[i][0];
-                new_neuron.cls = v[i][2];
-                new_neuron.r = rmax;
+                new_neuron.cls = v[i][vector_len];
+                new_neuron.r = a.rmax;
                 modif = true;
                 int pos = std::find(out_neurons.begin(), out_neurons.end(), new_neuron.cls) - out_neurons.begin();
                 if (pos >= out_neurons.size() ) {
@@ -111,7 +162,7 @@ int main()
         }
     }
     std::cout << std::endl << "--- End of learning phase ---" <<std::endl << std::endl;
-    if (vector_len == 2) {
+    if (a.wolfram && vector_len == 2) {
         std::cout << "WolframAlpha plot:" << std::endl;
         std::cout << "    plot ";
         int size = net.size();
@@ -120,10 +171,38 @@ int main()
             if (i != size-1)
                 std::cout << " and ";
         }
-        std::cout << std::endl;
+        std::cout << std::endl << std::endl;
     }
+    std::cout << "Hidden neuron count: " << net.size() << std::endl;
+    std::cout << "Resulting hidden neurons:" << std::endl;
     for (int i=0; i < net.size(); i++) {
-        std::cout << "Class: " << out_neurons[net[i].out_neuron] << std::endl;
+        std::cout << "Centre: ";
+        print_vector(net[i].pv, vector_len);
+        std::cout << ", class: " << net[i].cls;
+        std::cout << ", radius: " << net[i].r << std::endl;
     }
-    std::cout << "Net length: " << net.size() << std::endl;
+
+    while(true) {
+        bool hit = false;
+        std::cout << "Input vetor for classification (d=" << vector_len << "):" << std::endl;
+        std::vector<int> inputv;
+        read_vector(&inputv, vector_len);
+        std::cout << "Comparing existing neurons to the vector:" << std::endl;
+        for (int j=0; j<net.size(); j++) {
+            float distance = dist(&inputv[0], net[j].pv, vector_len);
+            std::cout << "Neuron: ";
+            print_vector(net[j].pv, vector_len);
+            std::cout << ", class: " << net[j].cls;
+            std::cout << ", radius: " << net[j].r;
+            std::cout << ", distance: " << distance << std::endl;
+            if (distance <= net[j].r) {
+                std::cout << "    Hit - class: " << net[j].cls << std::endl;
+                hit = true;
+                break;
+            }
+        }
+        if (!hit) {
+            std::cout << "Classification unsuccessful." << std::endl;
+        }
+    }
 }
